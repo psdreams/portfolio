1b2e4d84ca151dc58e5cbb168eecf3bdc34c8f9c
getmanele <getmanele@gmail.com>
2020-11-09 16:17:00 +0300

    Изминение паралакс меньше 992px выключаем

--- app/js/common.js
+++ app/js/common.js
@@ -3,27 +3,28 @@ $(document).ready(function () {
 	$(".typed").typed({
 		strings: ["Опыт более 5 лет.", "Гарантии и сроки прописаны в договоре.", "На связи 24/7."],
 		typeSpeed: 30,
-		backSpeed: 10,
+		backSpeed: 30,
 		loop: true,
 	});
 
 
+	if ( $(window).width() > 769) {
 
+		$('.mouse-parallax').on('mousemove', (e) => {
+			const x = e.pageX / $(window).width();
+			const y = e.pageY / $(window).width();
 
-	$('.mouse-parallax').on('mousemove', (e) => {
-		const x = e.pageX / $(window).width();
-		const y = e.pageY / $(window).width();
+			$('.mouse-parallax__background').css(
+				'transform',
+				'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)'
+			)
 
-		$('.mouse-parallax__background').css(
-			'transform',
-			'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)'
-		)
-
-		$('.mouse-parallax__content').css(
-			'transform',
-			'translate(' + x * 40 + 'px, ' + y * 40 + 'px)'
-		)
-	});
+			$('.mouse-parallax__content').css(
+				'transform',
+				'translate(' + x * 40 + 'px, ' + y * 40 + 'px)'
+			)
+		});
+	};
 	// mask form
 	$("#phone").mask("+7 (999) 999-99-99");
 	// mask form end
