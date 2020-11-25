7a3e421bd4799d3b9973e13222c6c8dc1352056a
getmanele <getmanele@gmail.com>
2020-11-25 12:12:34 +0300

    price редактирования блока цен

--- app/js/common.js
+++ app/js/common.js
@@ -258,48 +258,48 @@ document.getElementById("servises__list-more_item3").onclick = function () {
 
 
 // Маска модельного окна телефон
-// function mask(inputName, mask, evt) {
-// 	try {
-// 	  var text = document.getElementById(inputName);
-// 	  var value = text.value;
+function mask(inputName, mask, evt) {
+	try {
+	  var text = document.getElementById(inputName);
+	  var value = text.value;
 
-// 	  // If user pressed DEL or BACK SPACE, clean the value
-// 	  try {
-// 		var e = (evt.which) ? evt.which : event.keyCode;
-// 		if ( e == 46 || e == 8 ) {
-// 		  text.value = "";
-// 		  return;
-// 		}
-// 	  } catch (e1) {}
+	  // If user pressed DEL or BACK SPACE, clean the value
+	  try {
+		var e = (evt.which) ? evt.which : event.keyCode;
+		if ( e == 46 || e == 8 ) {
+		  text.value = "";
+		  return;
+		}
+	  } catch (e1) {}
 
-// 	  var literalPattern=/[0\*]/;
-// 	  var numberPattern=/[0-9]/;
-// 	  var newValue = "";
+	  var literalPattern=/[0\*]/;
+	  var numberPattern=/[0-9]/;
+	  var newValue = "";
 
-// 	  for (var vId = 0, mId = 0 ; mId < mask.length ; ) {
-// 		if (mId >= value.length)
-// 		  break;
+	  for (var vId = 0, mId = 0 ; mId < mask.length ; ) {
+		if (mId >= value.length)
+		  break;
 
-// 		// Number expected but got a different value, store only the valid portion
-// 		if (mask[mId] == '0' && value[vId].match(numberPattern) == null) {
-// 		  break;
-// 		}
+		// Number expected but got a different value, store only the valid portion
+		if (mask[mId] == '0' && value[vId].match(numberPattern) == null) {
+		  break;
+		}
 
-// 		// Found a literal
-// 		while (mask[mId].match(literalPattern) == null) {
-// 		  if (value[vId] == mask[mId])
-// 			break;
+		// Found a literal
+		while (mask[mId].match(literalPattern) == null) {
+		  if (value[vId] == mask[mId])
+			break;
 
-// 		newValue += mask[mId++];
-// 	  }
+		newValue += mask[mId++];
+	  }
 
-// 	  newValue += value[vId++];
-// 	  mId++;
-// 	}
+	  newValue += value[vId++];
+	  mId++;
+	}
 
-// 	text.value = newValue;
-//   } catch(e) {}
-//   }
+	text.value = newValue;
+  } catch(e) {}
+  }
 
 
 
